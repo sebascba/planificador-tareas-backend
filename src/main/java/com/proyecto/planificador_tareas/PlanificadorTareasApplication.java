3905e257590f61057fce7842e998ba96345182c9
JhohanSebastianCordobaP <scba246@gmail.com>
2026-09-15 17:08:09 -0500

    first commit

---
+++ src/main/java/com/proyecto/planificador_tareas/PlanificadorTareasApplication.java
@@ -0,0 +1,13 @@
+package com.proyecto.planificador_tareas;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanificadorTareasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PlanificadorTareasApplication.class, args);
+	}
+
+}
